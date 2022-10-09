7dac4d9af6bd019ea09e63f644b420c15f69fd4a
Roberto Morales <roberto@morales.com.co>
2022-10-09 10:02:13 -0500

    Formulario y botón agregado inicial

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Formulario = () => {
+    const [fruta, setFruta] = useState('');
+    const [descripcion, setDescription] = useState('');
+  return (
+    <div className='container mt-5'>
+        <h1 className='text-center'>CRUD BÁSICO</h1>
+        <hr/>
+        <form>
+            <input 
+            className='form-control mb-2'
+            type="text" 
+            placeholder='Ingrese Fruta'
+            ></input>
+            <input 
+            className='form-control mb-2'
+            type="text" 
+            placeholder='Ingrese Descripción'></input>
+            <button className='btn btn-primary btn-block'>Agregar</button>
+        </form>
+    </div>
+  )
+}
+
+export default Formulario
